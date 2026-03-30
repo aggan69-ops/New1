0b3e2f85038ae52d1ef9fa9e4b5af697162fe7d7
aggan69-ops <aggan69@gmail.com>
2026-03-30 20:43:23 +0200

    layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="sv">
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
